7f6bd5ccaefedc91c5bafe77e19c5db3a24970ad
LydeVN <julesvannelli@gmail.com>
2024-11-21 18:25:13 +0100

    init

---
+++ 241121/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+double celcius, faranheit;
+celcius = 65.8;
+faranheit = 45.2;
+string monPrenom;
+Console.WriteLine(celcius + faranheit);
+Console.WriteLine("Donne-moi ton prenom :");
+monPrenom = Console.ReadLine();
+Console.WriteLine("Bonjour "+monPrenom);
---
+++ library.cs
@@ -0,0 +1,7 @@
+// Les types 
+
+int age; //entier
+double prix; // Réel
+char caracter; // Charactère
+bool marie; // boolean
+string prenom; // chaine de charactère
